8a84eec4f2f93da9fd7422a0f6876f7a351574ac
Mike Vays <michailvays@gmail.com>
2024-06-02 23:33:02 +0700

    Implemented chat room for multiple clients

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -3,12 +3,21 @@ import java.io.*;
 
 public class ChatClient {
     private Socket socket = null;
+    private PrintWriter pr = null;
+    private InputStreamReader inputStream = null;
+    private BufferedReader in = null;
 
     ChatClient(String adress, int port) {
         try {
             socket = new Socket(adress, port);
-            System.out.println("Client connected");
-            
+            System.out.println("You entered a chat room");
+            pr = new PrintWriter(socket.getOutputStream(), true);
+            // TODO while message != "quit" read from buffer and display
+            pr.println("Hi brotha!");
+            inputStream = new InputStreamReader(socket.getInputStream());
+            in = new BufferedReader(inputStream);
+            System.out.println(in.readLine());
+
             socket.close();
         }
         catch (UnknownHostException u) {
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,15 +1,35 @@
 import java.net.*;
+import java.util.ArrayList;
 import java.io.*;
 
 public class ChatServer {
+    private static ArrayList<Socket> clients = new ArrayList<>();
+    private static PrintWriter pr = null;
+    private static InputStreamReader inputStream = null;
+    private static BufferedReader in = null;
+
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(6666);
         System.out.println("Waiting for clients...");
 
-        Socket client = server.accept();
-        System.out.println("Connection established " + client);
-
-        client.close();
-        server.close();
+        while (true) {
+            Socket clientSocket = server.accept();
+            System.out.println("Connection established " + clientSocket);
+            clients.add(clientSocket);
+            for (Socket client : clients) {
+                inputStream = new InputStreamReader(client.getInputStream());
+                in = new BufferedReader(inputStream);
+                String message = in.readLine();
+                // TODO if message == "quit" then clients.remove(client)
+                if (message != null) {
+                    for (int i = 0; i < clients.size(); i++) {
+                        Socket messageTo = clients.get(i);
+                        pr = new PrintWriter(messageTo.getOutputStream(), true);
+                        pr.println(client + " : " + message);
+                    }
+                }
+            }
+        }
     }
+
 }
