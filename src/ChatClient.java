7cf0b5c83e9396294d0105e9e26e71d0073f1127
Mike Vays <michailvays@gmail.com>
2024-06-03 22:08:35 +0700

    can't do it without threads, need to learn it first

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -6,23 +6,32 @@ public class ChatClient {
     private PrintWriter pr = null;
     private InputStreamReader inputStream = null;
     private BufferedReader in = null;
+    private BufferedReader consoleIn = null;
 
     ChatClient(String adress, int port) {
         try {
             socket = new Socket(adress, port);
             System.out.println("You entered a chat room");
+
             pr = new PrintWriter(socket.getOutputStream(), true);
-            // TODO while message != "quit" read from buffer and display
-            pr.println("Hi brotha!");
             inputStream = new InputStreamReader(socket.getInputStream());
             in = new BufferedReader(inputStream);
-            System.out.println(in.readLine());
 
+            String message;
+            consoleIn = new BufferedReader(new InputStreamReader(System.in));
+            while ((message = consoleIn.readLine()) != "!quit") {
+                pr.println(message);
+                System.out.println(in.readLine());
+            }
+
+            System.out.println("\nYou have disconnected from the chat...");
             socket.close();
         }
+
         catch (UnknownHostException u) {
             System.out.println("Host is unknown" + u.getMessage());
         }
+
         catch (IOException i) {
             System.out.println("Error occured: " + i.getMessage());
         }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -12,24 +12,33 @@ public class ChatServer {
         ServerSocket server = new ServerSocket(6666);
         System.out.println("Waiting for clients...");
 
-        while (true) {
+        while (!server.isClosed()) {
             Socket clientSocket = server.accept();
             System.out.println("Connection established " + clientSocket);
             clients.add(clientSocket);
+
             for (Socket client : clients) {
                 inputStream = new InputStreamReader(client.getInputStream());
                 in = new BufferedReader(inputStream);
                 String message = in.readLine();
-                // TODO if message == "quit" then clients.remove(client)
-                if (message != null) {
+                
+                if (!message.equals("") && !message.equals("!quit")) {
                     for (int i = 0; i < clients.size(); i++) {
                         Socket messageTo = clients.get(i);
                         pr = new PrintWriter(messageTo.getOutputStream(), true);
                         pr.println(client + " : " + message);
                     }
                 }
+
+                else if (message.equals("!quit")){
+                    for (int i = 0; i < clients.size(); i++) {
+                        Socket messageTo = clients.get(i);
+                        pr = new PrintWriter(messageTo.getOutputStream(), true);
+                        pr.println(client + " have disconnected from the chat...");
+                    }
+                    clients.remove(client);
+                }
             }
         }
     }
-
 }
