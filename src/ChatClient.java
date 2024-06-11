3c9bf9f5ae06dcc320d2682b337bdb354af202d5
MikeV182 <michailvays@gmail.com>
2024-06-11 20:25:41 +0700

    Implemented first working version of app using threads

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -8,36 +8,63 @@ public class ChatClient {
     private BufferedReader in = null;
     private BufferedReader consoleIn = null;
 
+    public void messageListener(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while(socket.isConnected()) {
+                        System.out.println(in.readLine());
+                    }
+                } catch (IOException i) {
+                    closeEverything();
+                }
+            }
+        }).start();
+    }
+
+    public void messageSender() {
+        try {
+            String message = "";
+            while (socket.isConnected()) {
+                message = consoleIn.readLine();
+                pr.println(message);
+            }
+        } catch (IOException i) {
+            closeEverything();
+        }
+    }
+
+    public void closeEverything() {
+        try {
+            socket.close();
+            pr.close();
+            inputStream.close();
+            in.close();
+            consoleIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+
     ChatClient(String adress, int port) {
         try {
             socket = new Socket(adress, port);
             System.out.println("You entered a chat room");
-
             pr = new PrintWriter(socket.getOutputStream(), true);
             inputStream = new InputStreamReader(socket.getInputStream());
             in = new BufferedReader(inputStream);
-
-            String message;
             consoleIn = new BufferedReader(new InputStreamReader(System.in));
-            while ((message = consoleIn.readLine()) != "!quit") {
-                pr.println(message);
-                System.out.println(in.readLine());
-            }
-
-            System.out.println("\nYou have disconnected from the chat...");
-            socket.close();
-        }
-
-        catch (UnknownHostException u) {
+        } catch (UnknownHostException u) {
             System.out.println("Host is unknown" + u.getMessage());
-        }
-
-        catch (IOException i) {
+        } catch (IOException i) {
             System.out.println("Error occured: " + i.getMessage());
         }
     }
 
     public static void main(String[] args) throws IOException {
         ChatClient client = new ChatClient("localhost", 6666);
+        client.messageListener();
+        client.messageSender();
     }
 }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -3,10 +3,7 @@ import java.util.ArrayList;
 import java.io.*;
 
 public class ChatServer {
-    private static ArrayList<Socket> clients = new ArrayList<>();
-    private static PrintWriter pr = null;
-    private static InputStreamReader inputStream = null;
-    private static BufferedReader in = null;
+    private static ArrayList<ClientHandler> clients = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(6666);
@@ -15,29 +12,49 @@ public class ChatServer {
         while (!server.isClosed()) {
             Socket clientSocket = server.accept();
             System.out.println("Connection established " + clientSocket);
-            clients.add(clientSocket);
+            
+            ClientHandler newClient = new ClientHandler(clientSocket, clients);
+            clients.add(newClient);
+            new Thread(newClient).start();
+        }
+    }
+}
 
-            for (Socket client : clients) {
-                inputStream = new InputStreamReader(client.getInputStream());
-                in = new BufferedReader(inputStream);
-                String message = in.readLine();
-                
-                if (!message.equals("") && !message.equals("!quit")) {
-                    for (int i = 0; i < clients.size(); i++) {
-                        Socket messageTo = clients.get(i);
-                        pr = new PrintWriter(messageTo.getOutputStream(), true);
-                        pr.println(client + " : " + message);
-                    }
-                }
+class ClientHandler implements Runnable {
+    private Socket cliSocket = null;
+    private ArrayList<ClientHandler> clients = new ArrayList<>();
+    private PrintWriter pr = null;
+    private InputStreamReader inputStream = null;
+    private BufferedReader in = null;
+    
+    ClientHandler(Socket cliSocket, ArrayList<ClientHandler> clients) throws IOException {
+        this.cliSocket = cliSocket;
+        this.clients = clients;
+        inputStream = new InputStreamReader(cliSocket.getInputStream());
+        in = new BufferedReader(inputStream);
+        pr = new PrintWriter(cliSocket.getOutputStream(), true);
+    }
 
-                else if (message.equals("!quit")){
-                    for (int i = 0; i < clients.size(); i++) {
-                        Socket messageTo = clients.get(i);
-                        pr = new PrintWriter(messageTo.getOutputStream(), true);
-                        pr.println(client + " have disconnected from the chat...");
-                    }
-                    clients.remove(client);
+    @Override
+    public void run() {
+        try {
+            String message = "";
+            while ((message = in.readLine()) != null) {
+                for (ClientHandler client : clients) {
+                    client.pr.println(cliSocket + ":" + message);
                 }
+            }
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+        finally {
+            try {
+                cliSocket.close();
+                pr.close();
+                inputStream.close();
+                in.close();
+            } catch (IOException i) {
+                i.printStackTrace();
             }
         }
     }
